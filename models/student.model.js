1bef321e97393913efb39be2c6f40c3c1e067c6c
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-11-02 16:42:12 +0100

    lecture done

--- models/student.model.js
+++ models/student.model.js
@@ -2,11 +2,25 @@ const mongoose = require('mongoose');
 
 // crear el esquema (las caracteristicas que tendrá cada documento)
 const studentSchema = new mongoose.Schema({
-  username: String,
-  candy: Number,
+  username: {
+    // determinar multiples validaciones de esta propiedad
+    type: String,
+    required: true, // esta propiedad es obligatoria
+    unique: true // el username no se puede repetir
+  },
+  candy: {
+    type: Number,
+    required: true,
+    default: 1 // en caso que no demos un valor, el sistema automaticamente coloca este valor
+  },
   likesPokemon: Boolean,
-  pizzaToppings: [ String ]
+  pizzaToppings: [ {
+    type: String,
+    enum: ["peperoni", "tomate", "piña", "jamon", "anchoas"] // enum es, los unicos posibles valores que puede haber en esta propiedad
+  } ]
 })
+
+// ! cada vez que nosotros modificamos el schema de una colleccion, debemos borrar y empezar de nuevo la coleccion.
 
 // crear el modelo => la manito que nos permitirá acceder a la DB para buscar data o hacer cambios. Se escriben en PascalCasing
 const Student = mongoose.model("Student", studentSchema)
--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 
 const Student = require("./models/student.model.js")
 
+// los archivos de JSON el sistema los exporta automaticamente.
+const allStudents = require("./data/students.json")
+
 mongoose.connect('mongodb://127.0.0.1:27017/my-first-database')
 .then(() => {
   console.log("conectados correctamente a la DB")
@@ -12,21 +15,45 @@ mongoose.connect('mongodb://127.0.0.1:27017/my-first-database')
   // cada vez que ejecutemos el codigo, lo que pongamos acá va a ocurrir
 
   // Create (Crear)
-  return Student.create({
-    username: "Chalo",
-    candy: 10,
-    likesPokemon: true,
-    pizzaToppings: ["peperoni", "jamon", "piña"]
-  })
-
+  // return Student.create({
+  //   username: "Chalo",
+  //   candy: 10,
+  //   likesPokemon: true,
+  //   pizzaToppings: ["peperoni", "jamon", "piña"]
+  // })
+  // return Student.insertMany(allStudents)
+  
   // Read (Leer)
+  //            query de mongo
+  //                   |
+  // return Student.find( { pizzaToppings: { $in: ["piña"] } } ) // busca todo lo que cumpla la condicion
+
+  // ejemplo de opciones de busqueda
+  // return Student.find( { likesPokemon: true } )
+  // .select( { username:1, candy: 1 } )
+  // .sort( { candy: 1 } )
+  // .skip(1)
+  // .limit(2)
+
+  // return Student.findOne( { username: "Adrian" } ) // devuelve un OBJ con el documento primer documento encontrado
+  // return Student.findById("6543b4ade85461c92405f3b9") // forma optimizada de mongo para buscar documentos
+
   // Update (Actualizar)
+  // return Student.findOneAndUpdate( { username: "Chalo" }, { $pull: {pizzaToppings: "jamon"}, candy: 30 }, {new: true} )
+  // dos argumentos
+  // 1. la busqueda que hacemos
+  // 2. el cambio que queremos hacer
+  // POR ALGUNA RAZON EXTRAÑA DE MONGO, los metodos de actualizar, nos devuelven el documento desactualizado. Para cambiar este comportamiento, debemos agregarle un 3r argumento { new: true }
+
   // Delete (Borrar)
+  return Student.findByIdAndDelete("6543b4ade85461c92405f3b7")
+
 
 
 })
-.then(() => {
-  console.log("nuevo estudiante creado")
+.then((response) => {
+  // console.log("varios estudiantes añadidos a la DB")
+  console.log("documento borrado", response)
 })
 .catch((error) => {
   console.log(error)
